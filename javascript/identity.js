457db931297f6683f4e5dd9d798acbd2759e4ad8
Mike De Wolfe <dewolfe.michael@gmail.com>
2015-11-16 16:29:16 +0400

    Typo

--- javascript/identity.js
+++ javascript/identity.js
@@ -332,7 +332,7 @@ function _barcodeScanCallback(responseString) {
         		}
         	};
 
-        	var messageHandler = nativeHandler.messageHanlers[HandlerNames.barcode_handler];
+        	var messageHandler = nativeHandler.messageHandlers[HandlerNames.barcode_handler];
         	_callNative(message,messageHandler);
 		},
 	};
